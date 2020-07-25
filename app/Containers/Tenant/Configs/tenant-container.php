eace0947b8d74cf0e166fac8cc8b550b3b625945
Syed Ali Murtaza <smurtazakazmi14@gmail.com>
2020-07-25 13:44:12 +0500

    Adds configurations and config checks mentionesd in #13.

--- app/Containers/Tenant/Configs/tenant-container.php
+++ app/Containers/Tenant/Configs/tenant-container.php
@@ -2,13 +2,49 @@
 
 return [
 
-    /*
+  /*
     |--------------------------------------------------------------------------
-    | Tenant Container
+   ", "Multi-Tenancy Enables
     |--------------------------------------------------------------------------
     |
-    |
+   ", "We may want to exclude few tables to use tenant_id
     |
     */
+
+    'enabled' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+   ", "Ignore tables list
+    |--------------------------------------------------------------------------
+    |
+   ", "We may want to exclude few tables to use tenant_id
+   ", "- It is required to skip these tables in migration, while adding tenant_id column
+   ", "- It is required to skip tenant_id global check, while quering on these tables
+   ", "- It is required to skip tenant_id column value set, while creating record on these tables
+    |
+    */
+
+    'ignore_tables' => [
+      "failed_jobs",
+      "migrations",
+
+      "oauth_access_tokens",
+      "oauth_auth_codes",
+      "oauth_clients",
+      "oauth_personal_access_clients",
+      "oauth_refresh_tokens",
+      "password_resets",
+
+      "roles",
+      "permissions",
+      "role_has_permissions",
+      "model_has_permissions",
+      "model_has_roles",
+
+      "tenants",
+    ]
+
+
 
 ];
--- app/Ship/core/Traits/MultiTenantableTrait.php
+++ app/Ship/core/Traits/MultiTenantableTrait.php
@@ -14,17 +14,22 @@ trait MultiTenantableTrait {
 
     if (auth()->check()) {
       static::creating(function ($model) {
-        $model->tenant_id = auth()->tenant_id();
+        if(Config::get('tenant-container.enabled') && !in_array($model->getTable(), Config::get('tenant-container.ignore_tables'))){
+          $model->tenant_id = auth()->tenant_id();
+        }
       });
 
       // if user is not administrator - role_id 1
       if (auth()->user()->role_id != 1) {
         static::addGlobalScope('tenant_id', function (Builder $builder) {
-          $builder->where('tenant_id', auth()->tenant_id());
+          if(Config::get('tenant-container.enabled') && !in_array($builder->model->getTable(), Config::get('tenant-container.ignore_tables'))){
+            $builder->where('tenant_id', auth()->tenant_id());
+          }
         });
       }
     }
-
   }
+
+  // TODO: Handle tenant_id globally while creating new record
 
 }
