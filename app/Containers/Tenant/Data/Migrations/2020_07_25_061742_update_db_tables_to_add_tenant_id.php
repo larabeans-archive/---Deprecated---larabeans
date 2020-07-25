73ed6f6144169fc131a5cbad22f24e05f28d043f
Syed Ali Murtaza <smurtazakazmi14@gmail.com>
2020-07-25 13:50:37 +0500

    Ref #13, implements migration to  update tables to add tenant id column.

---
+++ app/Containers/Tenant/Data/Migrations/2020_07_25_061742_update_db_tables_to_add_tenant_id.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+
+class UpdateDbTablesToAddTenantId extends Migration
+{
+
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+      $tables = DB::select('SHOW TABLES');
+      foreach ($tables as $table) {
+        foreach ($table as $key => $name) {
+          if(Config::get('tenant-container.enabled') && !in_array($name, Config::get('tenant-container.ignore_tables'))){
+            Schema::table($name, function (Blueprint $t) {
+              if (Schema::hasColumn($t->getTable(), 'id')) {
+                $t->uuid('tenant_id')->after('id');
+              } else {
+                $t->uuid('tenant_id');
+              }
+            });
+          }
+        }
+      }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+      $tables = DB::select('SHOW TABLES');
+      foreach ($tables as $table) {
+        foreach ($table as $key => $name)
+          if(Config::get('tenant-container.enabled') && !in_array($name, Config::get('tenant-container.ignore_tables'))){
+            Schema::table($name, function (Blueprint $t) {
+              if (Schema::hasColumn($t->getTable(), 'tenant_id')) {
+                $t->dropColumn('tenant_id');
+              }
+            });
+          }
+      }
+    }
+}
