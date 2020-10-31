f55126c7a5a3cc09a14617ffc59c16c4fa9b21ca
Syed Ali Murtaza <ali@kazmi.me>
2020-10-31 14:28:28 +0500

    Updates create location task arguments, replaces data array with specific arguments

--- app/Containers/Location/Tasks/CreateLocationTask.php
+++ app/Containers/Location/Tasks/CreateLocationTask.php
@@ -17,8 +17,33 @@ class CreateLocationTask extends Task
         $this->repository = $repository;
     }
 
-    public function run(array $data)
+    public function run(
+        string $locatableType = null,
+        string $locatableId = null,
+        string $addressLine1 = null,
+        string $addressLine2 = null,
+        string $city = null,
+        string $stateId = null,
+        string $countryId = null,
+        string $postCode = null,
+        string $latitude = null,
+        string $longitude = null
+    )
     {
+
+        $data = [
+          'locatable_type' => $locatableType,
+          'locatable_id'    => $locatableId,
+          'address_line_1' => $addressLine1,
+          'address_line_2' => $addressLine2,
+          'city'           => $city,
+          'state_id'       => $stateId,
+          'country_id'     => $countryId,
+          'post_code'      => $postCode,
+          'latitude'       => $latitude,
+          'longitude'      => $longitude
+        ];
+
         try {
             return $this->repository->create($data);
         }
