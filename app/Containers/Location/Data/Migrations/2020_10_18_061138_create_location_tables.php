b2598353121477ac8a59582a8c2cf9c9fee0a258
Syed Ali Murtaza <ali@kazmi.me>
2020-11-16 23:28:18 +0500

    Updates location migrations

--- app/Containers/Location/Data/Migrations/2020_10_18_061138_create_location_tables.php
+++ app/Containers/Location/Data/Migrations/2020_10_18_061138_create_location_tables.php
@@ -17,10 +17,20 @@ class CreateLocationTables extends Migration
             $table->uuidMorphs('locatable');
             $table->string('address_line_1');
             $table->string('address_line_2')->nullable()->default(null);
-            $table->string('city');
-            $table->string('state_id');
-            $table->string('country_id');
+            $table->int('city_id')->index('city_id_index');
+            $table->int('state_id')->index('state_id_index');
+            $table->int('country_id')->index('country_id_index');
             $table->string('post_code')->nullable()->default(null);
+            $table->string('latitude')->nullable()->default(null);
+            $table->string('longitude')->nullable()->default(null);
+            $table->timestamps();
+            //$table->softDeletes();
+        });
+
+        Schema::create('mobile_locations', function (Blueprint $table) {
+            $table->uuid('id')->primary('id');
+            $table->uuid('tenant_id')->index('tenant_id_index');
+            $table->uuidMorphs('locatable');
             $table->string('latitude')->nullable()->default(null);
             $table->string('longitude')->nullable()->default(null);
             $table->timestamps();
@@ -34,5 +44,6 @@ class CreateLocationTables extends Migration
     public function down()
     {
         Schema::dropIfExists('locations');
+        Schema::dropIfExists('mobile_locations');
     }
 }
