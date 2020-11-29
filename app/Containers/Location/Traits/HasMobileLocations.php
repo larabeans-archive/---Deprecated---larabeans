27e5bf2cd1ff4e75d7524e58f8ab2d910f304c8a
Syed Ali Murtaza <ali@kazmi.me>
2020-11-29 19:07:01 +0500

    Adds trait to handle entities with moveable locations

---
+++ app/Containers/Location/Traits/HasMobileLocations.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Containers\Location\Traits;
+
+use App\Containers\Location\Models\Location;
+
+trait HasLocations
+{
+
+  /**
+   * Get the entity's locations.
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+   */
+  public function locations()
+  {
+    return $this->morphMany(Location::class, 'locatable')->orderBy('created_at', 'desc');
+  }
+
+}
