ad09b220e462f3e364ff8f22e527c6319d52094f
Syed Ali Murtaza <ali@kazmi.me>
2020-12-01 22:04:42 +0500

    Implements create city, country, state and location tasks

---
+++ app/Containers/Location/Tasks/CreateCityTask.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Containers\Location\Tasks;
+
+use App\Containers\Location\Data\Repositories\CityRepository;
+use App\Ship\Parents\Tasks\Task;
+use phpDocumentor\Reflection\Types\String_;
+
+class CreateCityTask extends Task
+{
+    protected $repository;
+
+    public function __construct(CityRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function run(
+        int $id,
+        int $countryId,
+        int $stateId,
+        string $name,
+        string $latitude,
+        string $longitude
+    )
+    {
+        try {
+            $this->repository->create([
+                "id"         =>    $id,
+                "country_id" =>    $countryId,
+                "state_id"   =>    $stateId,
+                "name"       =>    $name,
+                "latitude"   =>    $latitude,
+                "longitude"  =>    $longitude
+            ]);
+        }
+        catch (Exception $exception) {
+          throw new CreateResourceFailedException();
+        }
+    }
+}
---
+++ app/Containers/Location/Tasks/CreateStateTask.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Containers\Location\Tasks;
+
+use App\Containers\Location\Data\Repositories\StateRepository;
+use App\Ship\Parents\Tasks\Task;
+
+class CreateStateTask extends Task
+{
+    protected $repository;
+
+    public function __construct(StateRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function run(
+      int $id,
+      int $countryId,
+      string $name,
+      string $code
+    )
+    {
+        try {
+            $this->repository->create([
+                "id"         =>    $id,
+                "country_id" =>    $countryId,
+                "name"       =>    $name,
+                "code"       =>    $code
+            ]);
+        }
+        catch (Exception $exception) {
+            throw new CreateResourceFailedException();
+        }
+    }
+}
