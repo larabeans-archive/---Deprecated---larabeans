a2ff357de02e1fbacfea6dde10b1507e52ec5ecc
Syed Ali Murtaza <ali@kazmi.me>
2020-12-11 21:30:14 +0500

    Adds find state tasks

---
+++ app/Containers/Location/Tasks/FindStateByNameTask.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Containers\Location\Tasks;
+
+use App\Containers\Location\Data\Repositories\StateRepository;
+use App\Ship\Exceptions\NotFoundException;
+use App\Ship\Parents\Tasks\Task;
+use Exception;
+
+class FindStateByIdTask extends Task
+{
+
+    protected $repository;
+
+    public function __construct(StateRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function run($id)
+    {
+        try {
+            return $this->repository->find($id);
+        }
+        catch (Exception $exception) {
+            throw new NotFoundException();
+        }
+    }
+}
