4821334d79088090caf3474ec0e5571972f1a1d6
Syed Ali Murtaza <ali@kazmi.me>
2021-02-24 17:14:31 +0500

    Updates location permissions

--- app/Containers/Location/Data/Seeders/LocationPermissionsSeeder.php
+++ app/Containers/Location/Data/Seeders/LocationPermissionsSeeder.php
@@ -10,25 +10,28 @@ class LocationPermissionsSeeder extends Seeder
     public function run()
     {
       // Default Location Management Permissions ----------------------------------------------------------
-      Apiato::call('Authorization@CreatePermissionTask', ['manage-locations', 'Manage Location Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['list-locations', 'View All Locations Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['view-location', 'View Location Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['delete-location', 'Delete Location Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['create-location', 'Create Location Permission.']);
-
-      Apiato::call('Authorization@CreatePermissionTask', ['manage-countries', 'Manage Country Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['view-country', 'View Country Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['delete-country', 'Delete Country Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['create-country', 'Create Country Permission.']);
-
-      Apiato::call('Authorization@CreatePermissionTask', ['manage-states', 'Manage States Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['view-state', 'View State Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['delete-state', 'Delete State Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['create-state', 'Create State Permission.']);
-
-      Apiato::call('Authorization@CreatePermissionTask', ['manage-cities', 'Manage Cities Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['view-city', 'View City Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['delete-city', 'Delete City Permissions.']);
-      Apiato::call('Authorization@CreatePermissionTask', ['create-city', 'Create City Permission.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['manage-locations', 'Manage Location Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['list-locations', 'View All Locations Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['view-location', 'View Location Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['delete-location', 'Delete Location Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['create-location', 'Create Location Permission.']);
+//
+//      Apiato::call('Authorization@CreatePermissionTask', ['manage-countries', 'Manage Country Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['view-country', 'View Country Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['update-country', 'Edit Country Permission.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['delete-country', 'Delete Country Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['create-country', 'Create Country Permission.']);
+//
+//      Apiato::call('Authorization@CreatePermissionTask', ['manage-states', 'Manage States Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['view-state', 'View State Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['update-state', 'Edit State Permission.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['delete-state', 'Delete State Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['create-state', 'Create State Permission.']);
+//
+//      Apiato::call('Authorization@CreatePermissionTask', ['manage-cities', 'Manage Cities Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['view-city', 'View City Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['update-city', 'Edit City Permission.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['delete-city', 'Delete City Permissions.']);
+//      Apiato::call('Authorization@CreatePermissionTask', ['create-city', 'Create City Permission.']);
     }
 }
